08feddec19d32ee4c57a7304b0840ee55a1bec68
georgina puig <georginaxcodes@gmail.com>
2020-07-31 00:16:35 -0500

    save username + id to newly created campground

--- README.md
+++ README.md
@@ -121,5 +121,5 @@
 ## Users + Campgrounds
 
 - [x] Prevent an unauthenticated user from creating a campground
-- [ ] Save username + id to newly created campground
+- [x] Save username + id to newly created campground
 
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -27,7 +27,11 @@ router.post('/', isLoggedIn, function(req, res) {
   const name = req.body.name;
   const image = req.body.image;
   const desc = req.body.description; // input name="description"
-  const newCampground = {name: name, image: image, description: desc};
+  const author = {
+    id: req.user._id,
+    username: req.user.username
+  };
+  const newCampground = {name: name, image: image, description: desc, author: author};
   // create a new campground and save to db
   Campground.create(newCampground, function(err, newlyCreated) {
     if (err) {
