bd42b95c5c43721d6050596e89880efc4f0f95d4
georgina puig <georginaxcodes@gmail.com>
2020-08-07 20:03:23 -0500

    add author to seeds

--- seeds.js
+++ seeds.js
@@ -1,66 +1,79 @@
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
-const Comment = require("./models/comment");
+const Comment   = require("./models/comment");
  
-var data = [
+const data = [
   {
     name: "Cloud's Rest", 
     image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    author:{
+        id : "588c2e092403d111454fff76",
+        username: "Jack"
+    }
   },
   {
     name: "Desert Mesa", 
     image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    author:{
+        id : "588c2e092403d111454fff71",
+        username: "Jill"
+    }
   },
   {
     name: "Canyon Floor", 
     image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    author:{
+        id : "588c2e092403d111454fff77",
+        username: "Jane"
+    }
   }
 ];
  
 function seedDB(){
-  // remove all campgrounds
-  Campground.remove({}, function(err){
-    if(err){
-      console.log(err);
-    }
-    console.log("removed campgrounds!");
-
-    Comment.remove({}, function(err) {
-      if(err){
-        console.log(err);
-      }
-      console.log("removed comments!");
-      // add a few campgrounds
-      data.forEach(function(seed) {
-        Campground.create(seed, function(err, campground){
-          if (err){
+   //Remove all campgrounds
+   Campground.deleteMany({}, function(err){
+        if (err){
             console.log(err);
-          } else {
-            console.log("added a campground");
-            // create a comment
-            Comment.create(
-              {
-                text: "This place is great, but I wish there was internet",
-                author: "Homer"
-              }, function(err, comment) {
-                if(err){
-                  console.log(err);
-                } else {
-                  campground.comments.push(comment);
-                  campground.save();
-                  console.log("Created new comment");
-                }
-              }
-            );
-          }
+        }
+        console.log("removed campgrounds!");
+        Comment.deleteMany({}, function(err) {
+            if (err){
+                console.log(err);
+            }
+            console.log("removed comments!");
+            //add a few campgrounds
+            data.forEach(function(seed){
+                Campground.create(seed, function(err, campground){
+                    if(err){
+                        console.log(err);
+                    } else {
+                        console.log("added a campground");
+                        //create a comment
+                        Comment.create(
+                            {
+                                text: "This place is great, but I wish there was internet",
+                                author:{
+                                    id : "588c2e092403d111454fff76",
+                                    username: "Jack"
+                                }
+                            }, function(err, comment){
+                                if(err){
+                                    console.log(err);
+                                } else {
+                                    campground.comments.push(comment);
+                                    campground.save();
+                                    console.log("Created new comment");
+                                }
+                            });
+                    }
+                });
+            });
         });
-      });
-    });
-  }); 
-  //add a few comments
+    }); 
+    //add a few comments
 }
  
 module.exports = seedDB;
