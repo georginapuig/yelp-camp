f50d8ad8b7ea031eda03107942c3406081e4b4d6
georgina puig <georginaxcodes@gmail.com>
2020-08-23 00:42:11 -0500

    add req.flash

--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,7 @@ const router = express.Router({mergeParams: true});
 
 const Campground = require('../models/campground');
 const Comment    = require('../models/comment');
-const middleware = require("../middleware");
+const middleware = require('../middleware');
 
 // NEW
 router.get('/new', middleware.isLoggedIn, function(req, res) {
@@ -28,6 +28,7 @@ router.post('/', middleware.isLoggedIn, function(req, res) {
       // create new comment
       Comment.create(req.body.comment, function(err, comment) {
         if (err) {
+          req.flash('error', 'Something went wrong');
           console.log(err);
         } else {
           //add username and id to comment
@@ -40,7 +41,7 @@ router.post('/', middleware.isLoggedIn, function(req, res) {
           campground.save();
 
           console.log(comment);
-          
+          req.flash('success', 'Successfully added comment');
           // redirect campgound show page 
           res.redirect('/campgrounds/' + campground._id);
         }
@@ -53,33 +54,34 @@ router.post('/', middleware.isLoggedIn, function(req, res) {
 router.get('/:comment_id/edit', middleware.checkCommentOwnership, function(req, res) {
   Comment.findById(req.params.comment_id, function(err, foundComment) {
     if (err) {
-      res.redirect("back");
+      res.redirect('back');
     } else {
-      res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+      res.render('comments/edit', {campground_id: req.params.id, comment: foundComment});
     }
   });
 });
 
 // UPDATE
-router.put("/:comment_id", middleware.checkCommentOwnership, function(req, res){
+router.put('/:comment_id', middleware.checkCommentOwnership, function(req, res){
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
      if(err){
-         res.redirect("back");
+         res.redirect('back');
      } else {
-         res.redirect("/campgrounds/" + req.params.id );
+         res.redirect('/campgrounds/' + req.params.id );
      }
   });
 });
 
 // DESTROY
-router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, res){
+router.delete('/:comment_id', middleware.checkCommentOwnership, function(req, res){
   //findByIdAndRemove
   Comment.findByIdAndRemove(req.params.comment_id, function(err){
-     if(err){
-         res.redirect("back");
-     } else {
-         res.redirect("/campgrounds/" + req.params.id);
-     }
+    if(err){
+      res.redirect('back');
+    } else {
+      req.flash('success', 'Comment deleted');
+      res.redirect('/campgrounds/' + req.params.id);
+    }
   });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -22,10 +22,11 @@ router.post('/register', function(req, res) {
 
   User.register(newUser, req.body.password, function(err, user) {
     if (err) {
-      console.log(err);
-      return res.render('register');
+      req.flash('error', err.message);
+      return res.redirect('/register');
     } else {
       passport.authenticate('local')(req, res, function() {
+        req.flash('success', 'Welcome to YelpCamp ' + user.username);
         res.redirect('/campgrounds');
       });
     }
@@ -54,13 +55,5 @@ router.get('/logout', function(req, res) {
   req.flash("success", "Logged you out!");
   res.redirect('/campgrounds');
 });
-
-// MIDDLEWARE
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/login');
-}
 
 module.exports = router;
